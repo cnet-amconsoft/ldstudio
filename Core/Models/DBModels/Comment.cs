567143d3131554dad58d169ee8a6bef32bb5ad33
Alexander Petrov <cnet.amconsoft@gmail.com>
2019-02-03 14:43:28 +0200

    init

---
+++ Core/Models/DBModels/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models.DBModels
+{
+    public class Comment
+    {
+        public Comment() { }
+
+        public string Author { get; set; }
+        public string Body { get; set; }
+        public int CommentId { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ DaL/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Core.Models.DBModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DaL
+{
+     public class AppDbContext : DbContext
+    {
+        public DbSet<Comment> Comments { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
